2de374006101eb1ad8d1f78b14dc92ff2d45edf3
MikazukiLaisa <MikazukiLaisa>
2019-05-27 00:15:47 +0900

    とりあえず完成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import top1 from "./resources/top1.jpg";
 import top2 from "./resources/top2.jpg";
 import laisaIcon from "./resources/laisa.png";
+import twitter from "./resources/twitter4.png";
+import github from "./resources/github3.png";
+import hatebu from "./resources/hatebu.svg";
+import qiita from "./resources/qiita.png";
+import pixiv from "./resources/pixiv.png";
+import ff14 from "./resources/ff14_2.png";
+
 
 function App() {
   return (
@@ -22,18 +28,18 @@ class Main extends React.Component {
           <img src={top2} alt="post apocalypse"/>
         </div>
         <hr />
-        <div class="raison">
+        <div class="scroll raison">
           <h1>--raison d'être--</h1>
-          <p>To make my dream come true</p>
+          <p>To enjoy now.</p>
         </div>
         <hr />
-        <div class="dream">
+        <div class="scroll dream">
           <h1>--dream--</h1>
-          <p>Illustlrator</p>
+          <p>Illustrator</p>
           <p>proProgrammer</p>
         </div>
         <hr />
-        <div class="profile0">
+        <div class="scroll profile0">
           <h1>--profile--</h1>
           <div class="profile1">
             <img src={laisaIcon} alt="laisa" />
@@ -45,12 +51,37 @@ class Main extends React.Component {
           </div>
         </div>
         <hr />
-        <div class="links">
-          <h1>-links-</h1>
+        <div class="scroll links0">
+          <h1>--links--</h1>
+          <div class="links1">
+            <Button css="links2" img={twitter} link="https://twitter.com/Mikazuki_Laisa?lang=ja" alt="twitter" />
+            <Button css="links2" img={github} link="https://github.com/MikazukiLaisa" alt="github" />
+            <Button css="links2" img={qiita} link="https://qiita.com/Mikazuki_Laisa" alt="qiita" />
+            <Button css="links2" img={hatebu} link="http://b.hatena.ne.jp/Mikazuki_Laisa/bookmark" alt="hatena_bookmark" />
+            <Button css="links2" img={pixiv} link="https://www.pixiv.net/member.php?id=8383999" alt="pixiv" />
+            <Button css="links2" img={ff14} link="https://jp.finalfantasyxiv.com/lodestone/character/23125854/" alt="ff14" />
+          </div>
+        </div>
+        <hr />
+        <div class="footer">
+          <h2>Mikazuki Laisa 2019 All right reserved.</h2>
         </div>
       </div>
     )   
   }
 }
+
+class Button extends React.Component{
+  render(){
+    return(
+      <div class={this.props.css}>
+        <a href={this.props.link}>
+          <img src={this.props.img} alt={this.props.alt}/>
+        </a>
+    </div>
+    )
+  }
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ScrollLoad();
+
+
+function ScrollLoad(){
+    var scrollAnimationElm = document.querySelectorAll('.scroll');
+    console.log(scrollAnimationElm);
+    var scrollAnimationFunc = function() {
+      for(var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 0;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('show');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+    return null;
+  }
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
